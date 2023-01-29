d2ca36f4fc1a3e8c00d4bcbe5c9ebabf42ed6666
Aecy <devsnake@hotmail.com>
2023-01-29 06:14:26 +0100

    fix(typo): reward

--- app/Http/Controllers/Reward/RewardController.php
+++ app/Http/Controllers/Reward/RewardController.php
@@ -25,14 +25,18 @@ class RewardController extends Controller
         $user = $request->user();
         if (now()->diffInHours($user->last_reward) >= 24 || is_null($user->last_reward)) {
             $reward = $this->rewardService->getReward();
-            $user->messages_left += $reward;
-            $user->last_reward = now();
-            $user->save();
+            if ($reward > 0) {
+                $user->messages_left += $reward;
+                $user->last_reward = now();
+                $user->save();
 
-            return Redirect::back()->with('success', "Vous avez reçu {$reward} messages gratuitement ! Revenez demain pour recommencer.");
+                return Redirect::back()->with('success', "Vous avez reçu {$reward} crédits.");
+            }
+
+            return Redirect::back()->with('success', "Vous n'avez pas reçu de crédits malheureusement.");
         }
 
         $time = $user->last_reward->addHours(24);
-        return Redirect::back()->with('danger', "Revenez demain à {$time->format('H:i')} pour obtenir une nouvelle récompense.");
+        return Redirect::back()->with('danger', "Revenez le {$time->format('d à H:i')} pour obtenir une nouvelle récompense.");
     }
 }
