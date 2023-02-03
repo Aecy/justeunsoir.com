b77c33b3f39921e635b508a0ab19a46ffca06c1e
Aecy <devsnake@hotmail.com>
2023-02-03 17:30:16 +0100

    add(phpdoc): favorite controller

--- app/Http/Controllers/Favorite/FavoriteController.php
+++ app/Http/Controllers/Favorite/FavoriteController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Favorite;
 
 use App\Actions\Favorite\FavoriteAction;
-use App\Actions\Like\LikeAction;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
@@ -12,20 +11,34 @@ use Illuminate\View\View;
 
 class FavoriteController extends Controller
 {
+    /**
+     * Instancie la propriété toggle de la classe FavoriteAction.
+     *
+     * @param FavoriteAction $action
+     */
     public function __construct(
         private FavoriteAction $action
     ) { }
 
+    /**
+     * Toggle l'ajout ou le retrait d'un favoris pour un utilisateur.
+     *
+     * @param User $user
+     * @return RedirectResponse
+     */
     public function index(User $user): RedirectResponse
     {
-        $this->action->toggle(
-            $this->getUser(),
-            $user
-        );
+        $this->action->toggle($this->getUser(), $user);
 
         return Redirect::back();
     }
 
+    /**
+     * Affiche les favoris de l'utilisateur.
+     *
+     * @param User $user
+     * @return View
+     */
     public function show(User $user): View
     {
         return view('users.favorites', [
