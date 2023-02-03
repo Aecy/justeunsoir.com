fc8c1ce16bcddacbb88d46bdb6d6802c08446047
Aecy <devsnake@hotmail.com>
2023-02-03 17:30:23 +0100

    add(phpdoc): like controller

--- app/Http/Controllers/Like/LikeController.php
+++ app/Http/Controllers/Like/LikeController.php
@@ -9,10 +9,21 @@ use Illuminate\Http\RedirectResponse;
 
 class LikeController extends Controller
 {
+    /**
+     * Instancie la propriété toggle de la classe LikeAction.
+     *
+     * @param LikeAction $action
+     */
     public function __construct(
         private LikeAction $action,
     ) { }
 
+    /**
+     * Toggle l'ajout d'un cœur ou son retrait pour un utilisateur.
+     *
+     * @param User $user
+     * @return RedirectResponse
+     */
     public function index(User $user): RedirectResponse
     {
         $this->action->toggle($this->getUser(), $user);
