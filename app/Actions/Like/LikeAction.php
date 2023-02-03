0a1e317457827ebc4100293abb087c59979e0471
Aecy <devsnake@hotmail.com>
2023-02-03 17:27:54 +0100

    add(like): in conversation

--- app/Actions/Like/LikeAction.php
+++ app/Actions/Like/LikeAction.php
@@ -5,6 +5,7 @@ namespace App\Actions\Like;
 use App\Models\User;
 use App\Notifications\Like\DislikeNotification;
 use App\Notifications\Like\LikeNotification;
+use Musonza\Chat\Facades\ChatFacade as Chat;
 
 final class LikeAction
 {
@@ -17,9 +18,30 @@ final class LikeAction
         $sender->toggleLike($receiver);
 
         if ($sender->hasLiked($receiver)) {
+            $this->addLikeConversation($sender, $receiver);
             $receiver->notify(new LikeNotification($sender));
         } else {
+            $this->removeLikeConversation($sender, $receiver);
             $receiver->notify(new DislikeNotification($sender));
         }
+    }
+
+    private function addLikeConversation(User $sender, User $receiver): void
+    {
+        $conversation = Chat::conversations()->between($sender, $receiver);
+        if (is_null($conversation)) {
+            $conversation = Chat::createConversation([$sender, $receiver])->makeDirect();
+        }
+
+        Chat::message("Envoi d'un coeur")
+            ->type('like')
+            ->from($sender)
+            ->to($conversation)
+            ->send();
+    }
+
+    private function removeLikeConversation(User $sender, User $receiver)
+    {
+        // Nothing.
     }
 }
