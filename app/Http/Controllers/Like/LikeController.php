f0afe1192860dfdc9053d1b91d7cf10fd54a16ce
Aecy <devsnake@hotmail.com>
2023-02-03 11:13:11 +0100

    refactor(action): like

--- app/Http/Controllers/Like/LikeController.php
+++ app/Http/Controllers/Like/LikeController.php
@@ -6,20 +6,16 @@ use App\Actions\Like\LikeAction;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Redirect;
 
 class LikeController extends Controller
 {
     public function __construct(
-        private LikeAction $action
+        private LikeAction $action,
     ) { }
 
     public function index(User $user): RedirectResponse
     {
-        $this->action->toggle(
-            $this->getUser(),
-            $user
-        );
-        return Redirect::back();
+        $this->action->toggle($this->getUser(), $user);
+        return redirect()->back();
     }
 }
