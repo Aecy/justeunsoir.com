8605e35f9c6b09c74b541927e42be644a2eebd03
Aecy <devsnake@hotmail.com>
2023-02-03 17:31:34 +0100

    add(phpdoc): user media controller

--- app/Http/Controllers/User/MediaController.php
+++ app/Http/Controllers/User/MediaController.php
@@ -8,6 +8,12 @@ use Illuminate\View\View;
 
 class MediaController extends Controller
 {
+    /**
+     * Affiche les photos d'un utilisateur.
+     *
+     * @param User $user
+     * @return View
+     */
     public function show(User $user): View
     {
         return view('users.medias', [
