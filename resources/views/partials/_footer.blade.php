1980fa8b3bd6305b91823ee8b9af4331ac8ce232
Aecy <devsnake@hotmail.com>
2023-02-06 18:09:54 +0100

    refactor(view): size partials

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,105 +1,105 @@
 <footer class="footer-section">
-    <div class="footer-middle padding-tb" style="background-image: url({{ asset('assets/images/footer/bg.png') }});">
-        <div class="container">
-            <div class="row justify-content-around">
-                <div class="col-lg-4 col-md-6 col-12">
-                    <div class="footer-middle-item-wrapper">
-                        <div class="footer-middle-item mb-lg-0">
-                            <div class="fm-item-title">
-                                <h4>A propos de {{ config('app.name') }}</h4>
-                            </div>
-                            <div class="fm-item-content">
-                                <p class="mb-4">
-                                    Des rencontres sans attaches, des moments inoubliables:
-                                    Rejoignez notre communauté de personnes libres et ouvertes d'esprit pour des rencontres d'un soir.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-12">
-                    <div class="footer-middle-item-wrapper">
-                        <div class="footer-middle-item mb-lg-0">
-                            <div class="fm-item-title">
-                                <h4>Informations</h4>
-                            </div>
-                            <div class="fm-item-content">
-                                <div class="fm-item-widget lab-item">
-                                    <div class="lab-inner">
-                                        <div class="lab-content">
-                                            <h6>
-                                                <a href="{{ url('/') }}">Accueil</a>
-                                            </h6>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="fm-item-widget lab-item">
-                                    <div class="lab-inner">
-                                        <div class="lab-content">
-                                            <h6>
-                                                <a href="{{ route('search.index') }}">Recherches</a>
-                                            </h6>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="fm-item-widget lab-item">
-                                    <div class="lab-inner">
-                                        <div class="lab-content">
-                                            <h6>
-                                                <a href="{{ route('reward.index') }}">Récompenses</a>
-                                            </h6>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="fm-item-widget lab-item">
-                                    <div class="lab-inner">
-                                        <div class="lab-content">
-                                            <h6>
-                                                <a href="{{ route('shop.index') }}">Tarifs</a>
-                                            </h6>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-12">
-                    <div class="footer-middle-item-wrapper">
-                        <div class="footer-middle-item mb-lg-0">
-                            <div class="fm-item-title">
-                                <h4>Reste du site</h4>
-                            </div>
-                            <div class="fm-item-content">
-                                <div class="fm-item-widget lab-item">
-                                    <div class="lab-inner">
-                                        <div class="lab-content">
-                                            <h6>
-                                                <a href="{{ route('faq') }}">Foire aux questions</a>
-                                            </h6>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  <div class="footer-middle padding-tb" style="background-image: url({{ asset('assets/images/footer/bg.png') }});">
+    <div class="container">
+      <div class="row justify-content-around">
+        <div class="col-lg-4 col-md-6 col-12">
+          <div class="footer-middle-item-wrapper">
+            <div class="footer-middle-item mb-lg-0">
+              <div class="fm-item-title">
+                <h4>A propos de {{ config('app.name') }}</h4>
+              </div>
+              <div class="fm-item-content">
+                <p class="mb-4">
+                  Des rencontres sans attaches, des moments inoubliables:
+                  Rejoignez notre communauté de personnes libres et ouvertes d'esprit pour des rencontres d'un soir.
+                </p>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-    <div class="footer-bottom">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="footer-bottom-content text-center">
-                        <p>&copy; {{ now()->format('Y') }}
-                            <a href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a> - Tous droits réservés
-                        </p>
+        <div class="col-lg-4 col-md-6 col-12">
+          <div class="footer-middle-item-wrapper">
+            <div class="footer-middle-item mb-lg-0">
+              <div class="fm-item-title">
+                <h4>Informations</h4>
+              </div>
+              <div class="fm-item-content">
+                <div class="fm-item-widget lab-item">
+                  <div class="lab-inner">
+                    <div class="lab-content">
+                      <h6>
+                        <a href="{{ url('/') }}">Accueil</a>
+                      </h6>
                     </div>
+                  </div>
                 </div>
+                <div class="fm-item-widget lab-item">
+                  <div class="lab-inner">
+                    <div class="lab-content">
+                      <h6>
+                        <a href="{{ route('search.index') }}">Recherches</a>
+                      </h6>
+                    </div>
+                  </div>
+                </div>
+                <div class="fm-item-widget lab-item">
+                  <div class="lab-inner">
+                    <div class="lab-content">
+                      <h6>
+                        <a href="{{ route('reward.index') }}">Récompenses</a>
+                      </h6>
+                    </div>
+                  </div>
+                </div>
+                <div class="fm-item-widget lab-item">
+                  <div class="lab-inner">
+                    <div class="lab-content">
+                      <h6>
+                        <a href="{{ route('shop.index') }}">Tarifs</a>
+                      </h6>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
+        <div class="col-lg-4 col-md-6 col-12">
+          <div class="footer-middle-item-wrapper">
+            <div class="footer-middle-item mb-lg-0">
+              <div class="fm-item-title">
+                <h4>Reste du site</h4>
+              </div>
+              <div class="fm-item-content">
+                <div class="fm-item-widget lab-item">
+                  <div class="lab-inner">
+                    <div class="lab-content">
+                      <h6>
+                        <a href="{{ route('faq') }}">Foire aux questions</a>
+                      </h6>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+  <div class="footer-bottom">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="footer-bottom-content text-center">
+            <p>&copy; {{ now()->format('Y') }}
+              <a href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a> - Tous droits réservés
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </footer>
 
 
--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -1,75 +1,75 @@
 <header class="header-section">
-    <div class="header-bottom">
-        <div class="container">
-            <div class="header-wrapper">
-                <div class="logo">
-                    <a href="{{ url('/') }}">
-                        <img src="{{ asset('assets/images/logo/logo.png') }}" alt="logo">
-                    </a>
-                </div>
-                <div class="menu-area">
-                    <ul class="menu">
-                        <li>
-                            <a href="{{ url('/') }}">Accueil</a>
-                        </li>
-                        <li>
-                            <a href="{{ route('search.index') }}">Recherches</a>
-                        </li>
-                        <li>
-                            <a href="{{ route('reward.index') }}">Récompenses</a>
-                        </li>
-                        <li>
-                            <a href="{{ route('shop.index') }}">Tarifs</a>
-                        </li>
-                        @auth
-                            <li>
-                                <a href="#" class="">
-                                    <i class="icofont-user"></i> {{ auth()->user()->name }}
-                                </a>
-                                <ul class="submenu">
-                                    <li>
-                                        <a href="{{ route('dashboard') }}">
-                                            Mon compte
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ route('conversations.index') }}">
-                                            Mes messages
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <form class="d-none" method="POST" id="form-logout" action="{{ route('logout') }}">@csrf</form>
-                                        <a onclick="event.preventDefault(); document.getElementById('form-logout').submit()" href="#">
-                                            Se déconnecter
-                                        </a>
-                                    </li>
-                                </ul>
-                            </li>
-                        @else
-                            <li class="d-lg-none">
-                                <a href="{{ route('login') }}" class="">
-                                    Se connecter
-                                </a>
-                            </li>
-                        @endauth
-                    </ul>
-                    @guest
-                        <a href="{{ route('login') }}" class="login">
-                            <i class="icofont-user"></i>
-                            <span>Se connecter</span>
-                        </a>
-                        <a href="{{ route('register') }}" class="signup ms-2">
-                            <i class="icofont-users"></i>
-                            <span>S'enregistrer</span>
-                        </a>
-                    @endguest
-                    <div class="header-bar d-lg-none">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
-                </div>
-            </div>
+  <div class="header-bottom">
+    <div class="container">
+      <div class="header-wrapper">
+        <div class="logo">
+          <a href="{{ url('/') }}">
+            <img src="{{ asset('assets/images/logo/logo.png') }}" alt="logo">
+          </a>
         </div>
+        <div class="menu-area">
+          <ul class="menu">
+            <li>
+              <a href="{{ url('/') }}">Accueil</a>
+            </li>
+            <li>
+              <a href="{{ route('search.index') }}">Recherches</a>
+            </li>
+            <li>
+              <a href="{{ route('reward.index') }}">Récompenses</a>
+            </li>
+            <li>
+              <a href="{{ route('shop.index') }}">Tarifs</a>
+            </li>
+            @auth
+              <li>
+                <a href="#" class="">
+                  <i class="icofont-user"></i> {{ auth()->user()->name }}
+                </a>
+                <ul class="submenu">
+                  <li>
+                    <a href="{{ route('dashboard') }}">
+                      Mon compte
+                    </a>
+                  </li>
+                  <li>
+                    <a href="{{ route('conversations.index') }}">
+                      Mes messages
+                    </a>
+                  </li>
+                  <li>
+                    <form class="d-none" method="POST" id="form-logout" action="{{ route('logout') }}">@csrf</form>
+                    <a onclick="event.preventDefault(); document.getElementById('form-logout').submit()" href="#">
+                      Se déconnecter
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            @else
+              <li class="d-lg-none">
+                <a href="{{ route('login') }}" class="">
+                  Se connecter
+                </a>
+              </li>
+            @endauth
+          </ul>
+          @guest
+            <a href="{{ route('login') }}" class="login">
+              <i class="icofont-user"></i>
+              <span>Se connecter</span>
+            </a>
+            <a href="{{ route('register') }}" class="signup ms-2">
+              <i class="icofont-users"></i>
+              <span>S'enregistrer</span>
+            </a>
+          @endguest
+          <div class="header-bar d-lg-none">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </header>
