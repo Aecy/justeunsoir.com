a4e98016b31435f3af6495b962e973930a4c4e1e
Aecy <devsnake@hotmail.com>
2023-02-06 18:21:50 +0100

    refactor(page):; indent size 2

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -1,37 +1,41 @@
 @extends('layouts.master', ['title' => "Foire aux questions"])
 
 @section('content')
-    <section class="pricing-section padding-tb">
-        <div class="container">
-            <div class="section-header">
-                <h2 class="theme-color">Foire aux questions</h2>
-                <h4>Les questions fréquemment posées</h4>
-            </div>
-            <div class="section-wrapper mt-4">
-                <div class="pricing-plan-wrapper">
-                    <h5>S'inscrire sur {{ config('app.name') }}</h5>
-                    <p>
-                        Vous pouvez vous inscrire uniquement si vous avez plus de 18 ans ; dans le cas contraire. Nous vous invitons à passer votre chemin.
-                    </p>
-                    <hr class="text-muted">
-                    <h5>Activer votre compte</h5>
-                    <p>Vous devez vous inscrire et ensuite valider votre adresse e-mail pour profiter pleinement des fonctionnalités.</p>
-                    <hr class="text-muted">
-                    <h5>Oubli de mot de passe</h5>
-                    <p>Si vous avez oublié votre mot de passe, pas de problème ! <a href="{{ route('password.request') }}">Cliquez ici</a></p>
-                    <hr class="text-muted">
-                    <h5>Supprimer votre compte</h5>
-                    <p>Vous pouvez supprimer définitivement votre compte de {{ config('app.name') }}, aller sur "Mon Compte" puis "Sécurité" et supprimer votre compte en indiquant votre mot de passe actuel.</p>
-                    <hr class="text-muted">
-                    <h5>Concernant vos photos</h5>
-                    <p>
-                        Vos photos sont uniquement disponible sur site, aucun membre a le droit de prendre votre photo pour l'utilisé à des fins personnelles ou commerciales.
-                        <br>Les photos dénudées sont acceptées uniquement si vous avez plus de 18 ans.
-                    </p>
-                    <hr class="text-muted">
-                </div>
-            </div>
+  <section class="pricing-section padding-tb">
+    <div class="container">
+      <div class="section-header">
+        <h2 class="theme-color">Foire aux questions</h2>
+        <h4>Les questions fréquemment posées</h4>
+      </div>
+      <div class="section-wrapper mt-4">
+        <div class="pricing-plan-wrapper">
+          <h5>S'inscrire sur {{ config('app.name') }}</h5>
+          <p>
+            Vous pouvez vous inscrire uniquement si vous avez plus de 18 ans ; dans le cas contraire. Nous vous invitons
+            à passer votre chemin.
+          </p>
+          <hr class="text-muted">
+          <h5>Activer votre compte</h5>
+          <p>Vous devez vous inscrire et ensuite valider votre adresse e-mail pour profiter pleinement des
+            fonctionnalités.</p>
+          <hr class="text-muted">
+          <h5>Oubli de mot de passe</h5>
+          <p>Si vous avez oublié votre mot de passe, pas de problème ! <a href="{{ route('password.request') }}">Cliquez
+              ici</a></p>
+          <hr class="text-muted">
+          <h5>Supprimer votre compte</h5>
+          <p>Vous pouvez supprimer définitivement votre compte de {{ config('app.name') }}, aller sur "Mon Compte" puis
+            "Sécurité" et supprimer votre compte en indiquant votre mot de passe actuel.</p>
+          <hr class="text-muted">
+          <h5>Concernant vos photos</h5>
+          <p>
+            Vos photos sont uniquement disponible sur site, aucun membre a le droit de prendre votre photo pour
+            l'utilisé à des fins personnelles ou commerciales.
+            <br>Les photos dénudées sont acceptées uniquement si vous avez plus de 18 ans.
+          </p>
+          <hr class="text-muted">
         </div>
-
-    </section>
+      </div>
+    </div>
+  </section>
 @endsection
--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,40 +1,39 @@
 @extends('layouts.master', ['title' => 'Tarifs'])
 
 @section('content')
-    <section class="pricing-section" style="padding: 155px 0;">
-        <div class="container">
-            <div class="section-header">
-                <h4 class="theme-color">Nos packs</h4>
-                <h2>Tarifs des crédits</h2>
-            </div>
-            <div class="section-wrapper mt-4">
-                <div class="pricing-plan-wrapper">
-                    <div class="row gx-2 gy-3 justify-content-center">
-                        @foreach($products as $product)
-                            <div class="col-lg-4 col-sm-6">
-                                <div class="price-item">
-                                    <div class="price-item-inner">
-                                        <div class="price-top">
-                                            <h6>{{ $product->name }}</h6>
-                                            <h2>{{ number_format($product->price / 100, 2) }} €</h2>
-                                            <p>
-                                                <strong>{{ number_format($product->credits) }} crédits</strong>
-                                            </p>
-                                        </div>
-                                        <div class="price-bottom">
-                                            <a href="#" onclick="document.getElementById('paypal-{{ $product->id }}').submit()" class="purchase-btn">PayPal</a>
-                                            <a href="#" onclick="document.getElementById('stripe-{{ $product->id }}').submit()" class="purchase-btn">Carte bancaire</a>
-                                            <form id="stripe-{{ $product->id }}" class="d-none" method="post" action="{{ route('stripe.checkout', $product) }}">@csrf</form>
-                                            <form id="paypal-{{ $product->id }}" class="d-none" method="post" action="{{ route('paypal.checkout', $product) }}">@csrf</form>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        @endforeach
+  <section class="pricing-section" style="padding: 155px 0;">
+    <div class="container">
+      <div class="section-header">
+        <h4 class="theme-color">Nos packs</h4>
+        <h2>Tarifs des crédits</h2>
+      </div>
+      <div class="section-wrapper mt-4">
+        <div class="pricing-plan-wrapper">
+          <div class="row gx-2 gy-3 justify-content-center">
+            @foreach($products as $product)
+              <div class="col-lg-4 col-sm-6">
+                <div class="price-item">
+                  <div class="price-item-inner">
+                    <div class="price-top">
+                      <h6>{{ $product->name }}</h6>
+                      <h2>{{ number_format($product->price / 100, 2) }} €</h2>
+                      <p>
+                        <strong>{{ number_format($product->credits) }} crédits</strong>
+                      </p>
                     </div>
+                    <div class="price-bottom">
+                      <a href="#" onclick="document.getElementById('paypal-{{ $product->id }}').submit()" class="purchase-btn">PayPal</a>
+                      <a href="#" onclick="document.getElementById('stripe-{{ $product->id }}').submit()" class="purchase-btn">Carte bancaire</a>
+                      <form id="stripe-{{ $product->id }}" class="d-none" method="post" action="{{ route('stripe.checkout', $product) }}">@csrf</form>
+                      <form id="paypal-{{ $product->id }}" class="d-none" method="post" action="{{ route('paypal.checkout', $product) }}">@csrf</form>
+                    </div>
+                  </div>
                 </div>
-            </div>
+              </div>
+            @endforeach
+          </div>
         </div>
-
-    </section>
+      </div>
+    </div>
+  </section>
 @endsection
