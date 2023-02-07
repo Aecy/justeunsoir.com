89402f53b24eaaf9a28680ab97cb97f7fbf4b92c
Aecy <devsnake@hotmail.com>
2023-02-07 14:22:54 +0100

    add(data): acp datas temps

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\View\View;
 
 class AdminController extends Controller
 {
     public function index(): View
     {
-        return view('admin.index');
+        return view('admin.index', [
+            'data' => [
+                'ndd' => 100,
+                'host' => 100,
+                'total_revenu' => rand(20000, 80000),
+            ],
+            'products' => Product::all(),
+            'user' => $this->getUser()
+        ]);
     }
 }
