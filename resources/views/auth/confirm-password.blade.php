33c280f7dd000760f2348d4c96d19c8e7474af8f
Aecy <devsnake@hotmail.com>
2023-02-07 16:15:02 +0100

    fix(test): confirm password

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.master', ['title' => "Mot de passe oublié"])
+@extends('layouts.master', ['title' => "Confirmer le mot de passe"])
 
 @section('content')
   <section class="page-header-section style-1" style="background:url({{ asset('assets/images/page-header.jpg') }})">
@@ -6,7 +6,7 @@
       <div class="page-header-content">
         <div class="page-header-inner">
           <div class="page-title">
-            <h2>Mot de passe oublié</h2>
+            <h2>Confirmer le mot de passe</h2>
           </div>
         </div>
       </div>
@@ -16,58 +16,25 @@
   <div class="login-section padding-tb">
     <div class="container">
       <div class="account-wrapper">
-        <h3 class="title">Mot de passe oublié</h3>
-        <p>Vous avez oublié votre mot de passe ? Aucun problème. Indiquez-nous simplement votre adresse électronique et
-          nous vous enverrons un lien de réinitialisation du mot de passe qui vous permettra d'en choisir un
-          nouveau.</p>
-        <form class="account-form" method="POST" action="{{ route('password.email') }}">
+        <h3 class="title">Confirmer le mot de passe</h3>
+        <p>L'endroit de cette application est sécurisé. Confirmez votre mot de passe avant de continuer.</p>
+        <form class="account-form" method="POST" action="{{ route('password.confirm') }}">
           @csrf
           <div class="form-group">
-            <input type="email" placeholder="Adresse e-mail" name="email" value="{{ old('email') }}" autofocus>
-            @error("email")
-            <div class="text-danger align-items-start" style="display: block">
-              {{ $message }}
-            </div>
+            <input type="password" name="password" autofocus>
+            @error("password")
+              <div class="text-danger align-items-start" style="display: block">
+                {{ $message }}
+              </div>
             @enderror
           </div>
           <div class="form-group">
             <button type="submit" class="d-block lab-btn">
-              <span>Réinitialisation du mot de passe</span>
+              <span>Confirmer</span>
             </button>
           </div>
         </form>
-        @if(session('status'))
-          <span class="mb-4 text-success">{{ session('status') }}</span>
-        @endif
       </div>
     </div>
   </div>
 @endsection
-
-<x-guest-layout>
-  <div class="mb-4 text-sm text-gray-600">
-    {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
-  </div>
-
-  <form method="POST" action="{{ route('password.confirm') }}">
-  @csrf
-
-  <!-- Password -->
-    <div>
-      <x-input-label for="password" :value="__('Password')"/>
-
-      <x-text-input id="password" class="block mt-1 w-full"
-                    type="password"
-                    name="password"
-                    required autocomplete="current-password"/>
-
-      <x-input-error :messages="$errors->get('password')" class="mt-2"/>
-    </div>
-
-    <div class="flex justify-end mt-4">
-      <x-primary-button>
-        {{ __('Confirm') }}
-      </x-primary-button>
-    </div>
-  </form>
-</x-guest-layout>
