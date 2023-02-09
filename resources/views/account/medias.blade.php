0a9af2044e085ca13f71b23aa7f6c786811a1a3f
Aecy <devsnake@hotmail.com>
2023-02-09 13:49:35 +0100

    remove(header): account

--- resources/views/account/medias.blade.php
+++ resources/views/account/medias.blade.php
@@ -1,23 +1,6 @@
 @extends('layouts.master', ['title' => "Photos de mon compte"])
 
 @section('content')
-  <section class="page-header-section style-1" style="background:url({{ asset('assets/images/page-header.jpg') }})">
-    <div class="container">
-      <div class="page-header-content">
-        <div class="page-header-inner">
-          <div class="page-title">
-            <h2>Photos de mon compte</h2>
-          </div>
-          <ol class="breadcrumb">
-            <li><a href="{{ url('/') }}">Accueil</a></li>
-            <li><a href="#">{{ $user->name }}</a></li>
-            <li class="active">Photos</li>
-          </ol>
-        </div>
-      </div>
-    </div>
-  </section>
-
   <section class="profile-section padding-tb">
     <div class="container">
       <div class="section-wrapper">
