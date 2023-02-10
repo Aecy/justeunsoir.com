dcb6fdc427185274e506dd105e8f9b542b17484a
Aecy <devsnake@hotmail.com>
2023-02-10 21:19:11 +0100

    fix(register): redirect to mon-compte

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/account';
+    public const HOME = '/mon-compte';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
