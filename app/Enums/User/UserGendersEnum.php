7141f2d3398d8dede35d9e5daba15c75d8f07594
Aecy <devsnake@hotmail.com>
2023-02-12 18:43:38 +0100

    remove(gender): other gender

--- app/Enums/User/UserGendersEnum.php
+++ app/Enums/User/UserGendersEnum.php
@@ -6,7 +6,7 @@ enum UserGendersEnum: string
 {
     case Homme = 'man';
     case Femme = 'woman';
-    case Autre = 'other';
+    // case Autre = 'other';
 
     public static function random(): mixed
     {
